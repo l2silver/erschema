ec6306b6f00d6b2304b9d49837e43c3c90d94163
Leigh Silverstein <lsilversteinto@gmail.com>
2017-12-28 21:17:38 -0500

    add travis

--- src/index.js
+++ src/index.js
@@ -18,6 +18,26 @@ export type $normalizeResponse = {
   relationships: $relationships,
 };
 
+export const validateSchema = (schema: $schema)=>{
+  if (typeof schema !== 'object'){
+    throw new TypeError('schema must be an object')
+  }
+  Object.keys(schema).forEach(key => {
+    const entitySchema = schema[key];
+    if (entitySchema.relationships) {
+      entitySchema.relationships.forEach((relationship)=>{
+        if (!relationship.entityName) {
+          throw new TypeError(`${key} entitySchema is missing a relationship type for the relationship ${relationship.alias || relationship.name || 'Undefined'}`)
+        }
+        if (!relationship.type) {
+          throw new TypeError(`${key} entitySchema is missing a relationship type for the relationship ${relationship.alias || relationship.name || relationship.entityName}`)
+        }
+      })
+    }
+  })
+  return 'valid'
+}
+
 export default function normalize(
   input: Object,
   entityName: string,
